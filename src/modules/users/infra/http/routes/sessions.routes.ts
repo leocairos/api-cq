ee52b097cd5b4f4dc6468ed453e963897aa00134
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-11-11 09:41:49 -0300

    Insomnia and Routes

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,6 +1,9 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+
 import { celebrate, Segments, Joi } from 'celebrate';
 
+import logger from '@config/logger';
+import { remoteIp } from '@shared/services/util';
 import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
@@ -16,5 +19,20 @@ sessionsRouter.post(
   }),
   sessionsController.create,
 );
+
+const google = async (
+  request: Request,
+  response: Response,
+): Promise<Response> => {
+  logger.info(`POST in session/google (from ${remoteIp(request)})...`);
+
+  const { Entity, EntityId, ReferenceKey, Event } = request.body;
+
+  logger.info(JSON.stringify({ Entity, EntityId, ReferenceKey, Event }));
+
+  return response.status(200).json({ user: 1 });
+};
+
+sessionsRouter.post('/google', google);
 
 export default sessionsRouter;
